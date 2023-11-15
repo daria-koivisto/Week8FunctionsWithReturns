ddef6934f11842b83a740c91bc7d6e8861fdaed6
daria-koivisto <daria.koivisto@tktk.ee>
2023-11-15 14:35:54 +0200

    week8

---
+++ Week8FunctionsWithReturns/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] tvShows = { "Firefly", "Office", "The Last Of Us", "Friends", "Twin Peaks", "House", "Stranger Things" };
+string[] food = { "sushi", "pizza", "Chinese takeaway", "burger and chips", "chicken wings", "chips", "popcorn" };
+string[] drinks = { "mineral water", "Coca-Cola", "apple juice", "milk", "beer" };
+string randomMovie = PickRandomFromArray(tvShows);
+string randomFood = PickRandomFromArray(food);
+string randomDrink = PickRandomFromArray(drinks);
+Console.WriteLine($"Tonight I recommend you to watch {randomMovie}.");
+Console.WriteLine($"Order some {randomFood} and {randomDrink}.");
+
+static string PickRandomFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0, someArray.Length);
+    string randomElement = someArray[randomIndex];
+    return randomElement;
+}
